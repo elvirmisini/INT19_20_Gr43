8e3bc216793a5430efa64edae8392d8aae2c164f
elvirmisini <58117020+elvirmisini@users.noreply.github.com>
2020-01-03 19:31:18 +0100

    Add files via upload

--- projekti.js
+++ projekti.js
@@ -13,3 +13,33 @@ function Rmore() {
     moreText.style.display = "inline";
   }
 }
+function Rrmore() {
+  var dots = document.getElementById("dotss");
+  var moreText = document.getElementById("moree");
+  var btnText = document.getElementById("MEshumee");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more>>"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less<<"; 
+    moreText.style.display = "inline";
+  }
+}
+function Rrrmore() {
+  var dots = document.getElementById("dotsss");
+  var moreText = document.getElementById("moreee");
+  var btnText = document.getElementById("MEshumeee");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more>>"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less<<"; 
+    moreText.style.display = "inline";
+  }
+}
